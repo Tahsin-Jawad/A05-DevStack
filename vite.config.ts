8a89f6530c6cb63acc2275208cd2f647339b0367
Tahsin-Jawad <jawadtahsinal@gmail.com>
2026-09-13 02:14:31 +0600

    Fix base path for A05-DevStack repository

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs.plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react()],
+  base: '/A05-DevStack/', 
 })
